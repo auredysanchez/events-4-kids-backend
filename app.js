5392b29efb7f4883775d9e7479edd3764e78da49
Auredy Sanchez <auredysanchez@gmail.com>
2022-05-23 14:59:20 -0400

    creating post request

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const { response } = require("express");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-
-
 
 const eventsController = require("./controllers/eventsController");
 
 app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/events", eventsController);
 
--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { request, response } = require("../app");
 const router = express.Router();
 
 const KidsEvents = require("../kidsEvents.json");
@@ -16,6 +17,17 @@ router.get("/:id", (request, response) => {
   const singleEvent = events.find((event) => event.id === eventId); // find gives you the the object and the filter gives you the array within the object.
 
   response.json({ singleEvent }); // send the json for the student with the id of the url
+});
+
+router.post('/', (request, response) => {
+  const startAge = request.body.startAge;
+  const endAge = request.body.endAge;
+  const title = request.body.title;
+  const date = request.body.date;
+  const location = request.body.location;
+  
+  console.log(request.body);
+  response.json({success: true, message:'Event succussfully saved'})
 });
 
 module.exports = router;
