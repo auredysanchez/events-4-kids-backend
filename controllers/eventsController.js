55f75b72039600910865d9c56a4562bec58cc074
Auredy Sanchez <auredysanchez@gmail.com>
2022-06-04 17:20:16 -0400

    fixed the get request

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -11,22 +11,27 @@ router.get("/", (request, response) => {
 //get student by id
 router.get("/:id", (request, response) => {
   const eventId = request.params.id;
+  // console.log(eventId, 'id')
 
-  const events = KidsEvents.data;
+  const events = KidsEvents;
+  // console.log(KidsEvents.data, "le")
 
-  const singleEvent = events.find((event) => event.id === eventId); // find gives you the the object and the filter gives you the array within the object.
-
+  const singleEvent = events.find(event => event.id = eventId); // find gives you the the object and the filter gives you the array within the object.
+  console.log({singleEvent}, 'single')
   response.json({ singleEvent }); // send the json for the student with the id of the url
 });
 
 router.post('/', (request, response) => {
+  console.log(request.body, 'body')
   const startAge = request.body.startAge;
   const endAge = request.body.endAge;
   const title = request.body.title;
   const date = request.body.date;
   const location = request.body.location;
-  
-  console.log(request.body);
+
+  // const addEvent = {title, date, location, startAge, endAge}
+
+  console.log(request.body.addEvent);
   response.json({success: true, message:'Event succussfully saved'})
 });
 
